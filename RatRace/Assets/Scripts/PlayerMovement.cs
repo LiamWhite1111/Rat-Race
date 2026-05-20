d3183942a42e02f80c427bca4febfa51839b0081
AvocadoWitchKnits <knittingtogethersa@gmail.com>
2026-05-20 22:43:34 +0200

    Anne Assets Implemented

--- RatRace/Assets/Scripts/PlayerMovement.cs
+++ RatRace/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     float speedX, speedY;
     Rigidbody2D rb;
 
+    Animator anim; // Anne
+    private bool facingLeft = true; //Anne
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
      rb = GetComponent<Rigidbody2D>();   
-
+    anim = GetComponent<Animator>(); //Anne
     }
 
     // Update is called once per frame
@@ -20,5 +22,27 @@ public class PlayerMovement : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
         rb.linearVelocity = new Vector2(speedX, speedY);
+
+        Animate(); //Anne
+        if (speedX < 0 && !facingLeft || speedX > 0 && facingLeft)
+        {
+            Flip();
+        }
+    }
+
+    void Animate() //Anne
+    {
+        anim.SetFloat("MoveX", speedX);
+        anim.SetFloat("MoveY", speedY);
+       
+    }
+
+    void Flip() //Anne
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingLeft = !facingLeft;
+
     }
 }

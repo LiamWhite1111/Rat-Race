30623a1a0d93e63cb216333f09b6be159fe0598b
LiamWhite1111 <whiteliam451@gmail.com>
2026-05-31 20:16:35 +0200

    Final adjustmets

--- RatRace/Assets/Scripts/CatAI.cs
+++ RatRace/Assets/Scripts/CatAI.cs
@@ -7,12 +7,15 @@ public class CatAI : MonoBehaviour
     public float maxSpeed = 6f;
     private NavMeshAgent agent;
     private Transform target;
+    private Animator animator;
+    private Vector2 lastDirection = Vector2.down;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -28,6 +31,35 @@ public class CatAI : MonoBehaviour
             float t = 1 - Mathf.Clamp01(distance / 10f);
             agent.speed = Mathf.Lerp(minSpeed, maxSpeed, t);
             agent.SetDestination(target.position);
+
+            Vector2 velocity = agent.velocity;
+            bool isMoving = velocity.magnitude > 0.1f;
+            animator.SetBool("isWalking", isMoving);
+
+            if (isMoving)
+            {
+                lastDirection = velocity.normalized;
+
+                if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+                {
+                    animator.SetFloat("InputX", velocity.x > 0 ? 1 : -1);
+                    animator.SetFloat("InputY", 0);
+                    animator.SetFloat("LastInputX", velocity.x > 0 ? 1 : -1);
+                    animator.SetFloat("LastInputY", 0);
+                }
+                else
+                {
+                    animator.SetFloat("InputX", 0);
+                    animator.SetFloat("InputY", velocity.y > 0 ? 1 : -1);
+                    animator.SetFloat("LastInputX", 0);
+                    animator.SetFloat("LastInputY", velocity.y > 0 ? 1 : -1);
+                }
+            }
+            else
+            {
+                animator.SetFloat("InputX", 0);
+                animator.SetFloat("InputY", 0);
+            }
         }
     }
 }
--- RatRace/Assets/Scripts/DogAI.cs
+++ RatRace/Assets/Scripts/DogAI.cs
@@ -8,6 +8,8 @@ public class DogAI : MonoBehaviour
     private NavMeshAgent agent;
     private Transform target;
     private Vector3 spawnPosition;
+    private Animator animator;
+    private Vector2 lastDirection = Vector2.down;
 
     private void Awake()
     {
@@ -15,6 +17,7 @@ public class DogAI : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         agent.speed = dogSpeed;
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -35,6 +38,35 @@ public class DogAI : MonoBehaviour
             else
             {
                 agent.SetDestination(spawnPosition);
+            }
+
+            Vector2 velocity = agent.velocity;
+            bool isMoving = velocity.magnitude > 0.1f;
+            animator.SetBool("isWalking", isMoving);
+
+            if (isMoving)
+            {
+                lastDirection = velocity.normalized;
+
+                if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+                {
+                    animator.SetFloat("InputX", velocity.x > 0 ? 1 : -1);
+                    animator.SetFloat("InputY", 0);
+                    animator.SetFloat("LastInputX", velocity.x > 0 ? 1 : -1);
+                    animator.SetFloat("LastInputY", 0);
+                }
+                else
+                {
+                    animator.SetFloat("InputX", 0);
+                    animator.SetFloat("InputY", velocity.y > 0 ? 1 : -1);
+                    animator.SetFloat("LastInputX", 0);
+                    animator.SetFloat("LastInputY", velocity.y > 0 ? 1 : -1);
+                }
+            }
+            else
+            {
+                animator.SetFloat("InputX", 0);
+                animator.SetFloat("InputY", 0);
             }
         }
     }
